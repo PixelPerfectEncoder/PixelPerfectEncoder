08f869e44a3d5a42fc2315659eb6337ddae4049a
Zhonghao Luo <1999luozhonghao@gmail.com>
2023-10-21 22:48:16 -0400

    feat: add approximated residual block

--- PixelPerfect/Decoder.py
+++ PixelPerfect/Decoder.py
@@ -1,5 +1,6 @@
 from PixelPerfect.Yuv import YuvMeta, YuvFrame
 from PixelPerfect.Common import EncoderConfig
+from PixelPerfect.ResidualProcessor import ResidualProcessor
 import numpy as np
 
 class Decoder:
@@ -7,7 +8,8 @@ class Decoder:
         self.yuv_info = yuv_info
         self.encoder_config = encoder_config
         self.previous_frame = YuvFrame(np.full((self.yuv_info.height, self.yuv_info.width), 128))
-
+        self.residual_processor = ResidualProcessor()
+        
     def process(self, data):
         frame = np.zeros([self.yuv_info.height, self.yuv_info.width], dtype=np.uint8)
         block_size = self.encoder_config.block_size
@@ -18,7 +20,8 @@ class Decoder:
             col = seq % row_block_num * block_size
             
             frame[row:row + block_size, col:col + block_size] \
-                = self.previous_frame.data[ref_row:ref_row + block_size, ref_col:ref_col + block_size] + residual 
+                = self.previous_frame.data[ref_row:ref_row + block_size, ref_col:ref_col + block_size] \
+                    + self.residual_processor.decode(residual)
         self.previous_frame = YuvFrame(frame)
         return self.previous_frame
 
--- PixelPerfect/FileIO.py
+++ PixelPerfect/FileIO.py
@@ -10,20 +10,20 @@ def get_data_file_path(filename):
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(parent_dir, os.path.join('data', filename))
 
-def id2filename(id):
+def dataId2filename(id):
     return get_data_file_path(f"{id}.txt")
 
 def dump(data):
     id = uuid.uuid4().hex
-    with open(id2filename(id), 'wb') as file:
+    with open(dataId2filename(id), 'wb') as file:
         pickle.dump(data, file)
     return id
 
 def load(id):
-    with open(id2filename(id), 'rb') as file:
+    with open(dataId2filename(id), 'rb') as file:
         return pickle.load(file)
 
 def clean_data(file_ids):
     for id in file_ids:
-        os.remove(id2filename(id))
+        os.remove(dataId2filename(id))
     
