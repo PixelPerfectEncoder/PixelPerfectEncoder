66b107ecad1d0bf82bbe261cdf77fb50c4f7d3f8
Zhonghao Luo <1999luozhonghao@gmail.com>
2023-10-21 20:55:17 -0400

    feat: add file io

---
+++ PixelPerfect/FileIO.py
@@ -0,0 +1,29 @@
+import uuid
+import pickle
+import os
+
+def get_media_file_path(filename):
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(parent_dir, os.path.join('media', filename))
+
+def get_data_file_path(filename):
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(parent_dir, os.path.join('data', filename))
+
+def id2filename(id):
+    return get_data_file_path(f"{id}.txt")
+
+def dump(data):
+    id = uuid.uuid4().hex
+    with open(id2filename(id), 'wb') as file:
+        pickle.dump(data, file)
+    return id
+
+def load(id):
+    with open(id2filename(id), 'rb') as file:
+        return pickle.load(file)
+
+def clean_data(file_ids):
+    for id in file_ids:
+        os.remove(id2filename(id))
+    
--- PixelPerfect/Tests.py
+++ PixelPerfect/Tests.py
@@ -1,22 +1,22 @@
-import os
 from PixelPerfect.Decoder import Decoder
 from PixelPerfect.Encoder import Encoder, EncoderConfig
 from PixelPerfect.Yuv import YuvVideo, YuvMeta
-
-def get_file_path(filename):
-    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(parent_dir, os.path.join('media', filename))
+from PixelPerfect.FileIO import get_media_file_path, dump, load, clean_data
 
 def play_foreman_test():
     filename = 'foreman_cif-1.yuv'
     video_info = YuvMeta(height=288, width=352)
-    video = YuvVideo(get_file_path(filename), video_info)
+    video = YuvVideo(get_media_file_path(filename), video_info)
     config = EncoderConfig(block_size=16, block_search_offset=2)
     encoder = Encoder(video, config)
     decoder = Decoder(video_info, config)
+    file_ids = []
     for compressed_data in encoder.process():
-        decoded_frame = decoder.process(compressed_data)
+        file_id = dump(compressed_data)
+        file_ids.append(file_id)
+        decoded_frame = decoder.process(load(file_id))
         decoded_frame.display()
-        
+    clean_data(file_ids)
+
 def run_tests():
     play_foreman_test()
