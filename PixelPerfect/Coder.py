ff621ee0be9c79a46d5d465b2790207e313c70b0
Zhonghao Luo <1999luozhonghao@gmail.com>
2023-11-18 00:24:53 -0500

    fix a bug when the starting frame is p frame

--- PixelPerfect/Coder.py
+++ PixelPerfect/Coder.py
@@ -188,6 +188,7 @@ class VideoCoder(Coder):
         super().__init__(height, width, config)
         self.frame_seq = 0
         self.previous_frames: Deque[ReferenceFrame] = Deque(maxlen=config.nRefFrames)
+        self.previous_frames.append(ReferenceFrame(config, np.full(shape=(self.height, self.width), fill_value=128, dtype=np.uint8)))
         self.bitrate = 0
         
     def is_p_frame(self):
