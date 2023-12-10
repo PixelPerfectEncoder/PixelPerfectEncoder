8e2240717260fc74cceed5fd16916aee7d4ac738
Shamus <shamus@macbook-pro.home>
2023-12-10 16:43:45 -0500

    update Config file

--- PixelPerfect/CodecConfig.py
+++ PixelPerfect/CodecConfig.py
@@ -24,6 +24,7 @@ class CodecConfig:
         total_frames: int = 0,
         filename = '',
         is_firstpass = False,
+        dQPLimit: int = 0,
     ) -> None:
         self.block_size = block_size
         self.sub_block_size = block_size // 2
@@ -49,4 +50,4 @@ class CodecConfig:
         self.total_frames = total_frames
         self.filename = filename
         self.is_firstpass = is_firstpass
-        self.dQPLimit = dQPLimit  # Maximum allowed delta QP
+ 
